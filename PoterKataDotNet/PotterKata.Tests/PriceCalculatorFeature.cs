127a2abb13272d8fa1dc320db7e933329d4296e7
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-11 20:49:39 +0100

    :test_tube: red test: calcule_lowest_price

---
+++ PoterKataDotNet/PotterKata.Tests/PriceCalculatorFeature.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterKata.Tests
+{
+    [TestFixture]
+    public class PriceCalculatorFeature
+    {
+        [Test]
+        public void calcule_lowest_price()
+        {
+            var books = new string[] { "first_book", "second_book","third_book","fourth_book","fith_book",};
+
+            var price = PriceCalculator.Calcule(books);
+
+            price.Should().Be(51.20);
+        }
+
+    }
+}
