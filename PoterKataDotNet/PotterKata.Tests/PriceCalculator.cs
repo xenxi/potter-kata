cda9d2742603ec012886b81bfee7a6144034473c
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-11 11:21:37 +0100

    :recycle: refact

--- PoterKataDotNet/PotterKata.Tests/PriceCalculator.cs
+++ PoterKataDotNet/PotterKata.Tests/PriceCalculator.cs
@@ -4,12 +4,17 @@ namespace PotterKata.Tests
 {
     public class PriceCalculator
     {
-        private const int UnitPrice = 8;
+        private const decimal UnitPrice = 8;
 
         public static decimal Calcule(params string[] books)
         {
             if (books.Length > 1)
-                return 15.2m;
+            {
+                var subtotal = UnitPrice * books.Length;
+                var discount = (subtotal * 5) / 100;
+                return subtotal - discount;
+            }
+
             return UnitPrice;
         }
     }
