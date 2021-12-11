58bcd63e09f173b5fd044187000f8f2a79585c47
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-11 11:18:55 +0100

    :recycle: refact introduce constant

--- PoterKataDotNet/PotterKata.Tests/PriceCalculator.cs
+++ PoterKataDotNet/PotterKata.Tests/PriceCalculator.cs
@@ -4,11 +4,13 @@ namespace PotterKata.Tests
 {
     public class PriceCalculator
     {
+        private const int UnitPrice = 8;
+
         public static decimal Calcule(params string[] books)
         {
             if (books.Length > 1)
                 return 15.2m;
-            return 8;
+            return UnitPrice;
         }
     }
 }
