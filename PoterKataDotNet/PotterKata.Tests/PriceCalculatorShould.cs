b7249f12f0e41385ed9dd3aaaa9e9d859e820d72
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-11 10:53:00 +0100

    :white_check_mark: add test cases

--- PoterKataDotNet/PotterKata.Tests/PriceCalculatorShould.cs
+++ PoterKataDotNet/PotterKata.Tests/PriceCalculatorShould.cs
@@ -6,10 +6,16 @@ namespace PotterKata.Tests
     [TestFixture]
     public class PriceCalculatorShould
     {
-        [Test]
-        public void each_book_cost_eight()
+        [TestCase("first_book")]
+        [TestCase("second_book")]
+        [TestCase("third_book")]
+        [TestCase("fourth_book")]
+        [TestCase("fith_book")]
+        [TestCase("sixth_book")]
+        [TestCase("seventh_book")]
+        public void each_book_cost_eight(string book)
         { 
-            var price = PriceCalculator.Calcule("anyBook");
+            var price = PriceCalculator.Calcule(book);
 
             price.Should().Be(8);
         }
