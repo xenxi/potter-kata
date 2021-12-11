7d57dd5b16c5e2f4e1fcd701ccef70e7d1291d43
xenxi <antoniom.diaz.moreno@gmail.com>
2021-12-11 10:50:20 +0100

    :white_check_mark: green test: each_book_cost_eight

---
+++ PoterKataDotNet/PotterKata.Tests/PriceCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PotterKata.Tests
+{
+    public class PriceCalculator
+    {
+        public static int Calcule(string book)
+        {
+            return 8;
+        }
+    }
+}
---
+++ PoterKataDotNet/PotterKata.Tests/PriceCalculatorShould.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace PotterKata.Tests
+{
+    [TestFixture]
+    public class PriceCalculatorShould
+    {
+        [Test]
+        public void each_book_cost_eight()
+        { 
+            var price = PriceCalculator.Calcule("anyBook");
+
+            price.Should().Be(8);
+        }
+    }
+}
